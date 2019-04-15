2ac3840dc3e995cfc2cfc56580273a997755cc69
aledic85 <dicuollo85@gmail.com>
2019-04-15 15:16:13 +0200

    completed

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <!-- CSS: FONTAWESOME -->
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+     <!-- GOOGLE FONT : ROBOTO -->
+     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+     <!-- CSS: MY CSS -->
+     <link rel="stylesheet" href="style.css">
+     <!-- JS: JQUERY -->
+     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+     <!-- JS: MOMENT -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+     <!-- JS: CHART -->
+     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+     <!-- JS: HANDLEBARS -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.0/handlebars.min.js" charset="utf-8"></script>
+     <!-- JS: MY TEMPLATE -->
+     <script id="template" type="text/x-handlebars-template">
+     </script>
+     <!-- JS: MY JS -->
+     <script type="text/javascript" src="script.js"></script>
+     <title></title>
+   </head>
+   <body>
+
+     <?php
+
+       $nItems = $_GET["items"];
+       $myArr = [];
+       $sum = 0;
+
+       for ($i=0; $i < $nItems; $i++) {
+
+         $myNum = rand(1, 100);
+
+         $myArr[] = $myNum;
+         $sum += $myNum;
+       }
+
+       $max = $myArr[0];
+
+       for ($i=0; $i < sizeof($myArr); $i++) {
+
+         $elem = $myArr[$i];
+
+         if ($elem > $max) {
+
+           $max = $elem;
+         }
+       }
+
+       var_dump($myArr);
+       echo "<br>";
+       echo "La somma degli elementi degli array è $sum";
+       echo "<br>";
+       echo "Il numero più grande contenuto nell'array è $max";
+     ?>
+   </body>
+ </html>
